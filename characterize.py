e4c89acbd2b79d262a6d6f1ca7b33838118523da
shuoyangd <shuoyangd@gmail.com>
2018-11-02 16:30:57 -0400

    modify character to take care of non-ascii charactes

--- characterize.py
+++ characterize.py
@@ -1,13 +1,15 @@
+import re
 import sys
 
-def characterize(sent):
-  char_list = list(sent)
+def characterize(sent, split_regex="."):
+  splitter = re.compile(split_regex)
+  char_list = splitter.findall(sent)
   char_list = map(lambda c: c if c != " " else "<w/>", char_list)
   return " ".join(char_list)
 
 line = sys.stdin.readline()
 while line:
   char_line = characterize(line)
-  sys.stdout.write(char_line)
+  sys.stdout.write(char_line + "\n")
   line = sys.stdin.readline()
 

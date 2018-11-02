9f58345ec6c817a96369ad2917f283ca8a05a6d3
shuoyangd <shuoyangd@gmail.com>
2018-11-02 17:46:21 -0400

    improve non-ascii performance again

--- characterize.py
+++ characterize.py
@@ -4,8 +4,8 @@ import sys
 def characterize(sent, split_regex="."):
   splitter = re.compile(split_regex)
   char_list = splitter.findall(sent)
-  char_list = map(lambda c: c if c != " " else "<w/>", char_list)
-  return " ".join(char_list)
+  char_list = map(lambda c: c if c != " " else u"<w/>", char_list)
+  return u" ".join(char_list)
 
 line = sys.stdin.readline()
 while line:

3c5b5f439fb373e4f9582602e4cddd404b88fba0
shuoyangd <shuoyangd@gmail.com>
2018-11-06 15:41:19 -0500

    small fix for decharacterize

--- decharacterize.py
+++ decharacterize.py
@@ -1,13 +1,13 @@
 import sys
 
 def decharacterize(sent):
-  char_list = sent.split(u" ")
+  char_list = sent.strip().split(u" ")
   char_list = map(lambda c: c if c != u"<w/>" else u" ", char_list)
   return u"".join(char_list)
 
 line = sys.stdin.readline()
 while line:
   char_line = decharacterize(line)
-  sys.stdout.write(char_line)
+  sys.stdout.write(char_line + "\n")
   line = sys.stdin.readline()
 
